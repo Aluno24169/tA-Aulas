83e722ad50cef47994a1f974e6a1314fc57c33f4
Aluno24169 <aluno24169@ipt.pt>
2026-04-08 10:39:05 +0100

    Criação das classes Registration, Course , Student e MyUser

---
+++ Aulas/Aulas/Data/Model/Registration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Aulas.Data.Model
+{
+    /// <summary>
+    /// Classe do relacionamento entre alunos e disciplinas
+    /// </summary>
+    /// 
+    [PrimaryKey(nameof(StudentFK),nameof(CourseFK))] // PK composta (EF Core >=7)
+    public class Registration
+    {
+        public DateTime RegistrationDate { get; set; }
+
+        //FK para Student
+        //[Key, Column(Order = 1)] --> Válido para EF <=6
+        public int StudentFK { get; set; }
+        public Student Student { get; set; } = null!;
+
+        //FK para Course
+        //[Key, Column(Order = 2)] --> Válido para EF <=6
+        public int CourseFK { get; set; }
+        public Course Course { get; set; } = null!;
+    }
+}
